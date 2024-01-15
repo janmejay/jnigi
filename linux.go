a9673380fa6920f8e823f4d0b755846df98470ac
Shubham Jadhav <shubham.jadhav@rubrik.com>
2024-01-15 11:53:39 +0530

    Remove unloadjvm api

--- linux.go
+++ linux.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build linux
 // +build linux
 
 package jnigi
@@ -32,13 +33,9 @@ jint dyn_JNI_CreateJavaVM(JavaVM **pvm, void **penv, void *args) {
 import "C"
 
 import (
-	"unsafe"
 	"errors"
+	"unsafe"
 )
-
-type LibJVMPtr struct {
-	p unsafe.Pointer
-}
 
 func jni_GetDefaultJavaVMInitArgs(args unsafe.Pointer) jint {
 	return jint(C.dyn_JNI_GetDefaultJavaVMInitArgs((unsafe.Pointer)(args)))
@@ -56,16 +53,22 @@ func unloadLibJvm(p unsafe.Pointer) error {
 	return nil
 }
 
-func (p *LibJVMPtr) Unload() error {
-	return unloadLibJvm(unsafe.Pointer(p.p))
-}
+// Removing unload jvm api as it can only be called until the JVM is loaded.
+// Unloading the JVM is not supported after it is loaded.
+//type LibJVMPtr struct {
+//	p unsafe.Pointer
+//}
+//
+//func (p *LibJVMPtr) Unload() error {
+//	return unloadLibJvm(unsafe.Pointer(p.p))
+//}
 
-func LoadJVMLib(jvmLibPath string) (*LibJVMPtr, error) {
+func LoadJVMLib(jvmLibPath string) error {
 	cs := cString(jvmLibPath)
 	defer free(cs)
 	libHandle := uintptr(C.dlopen((*C.char)(cs), C.RTLD_NOW|C.RTLD_GLOBAL))
 	if libHandle == 0 {
-		return nil, errors.New("could not dynamically load libjvm.so")
+		return errors.New("could not dynamically load libjvm.so")
 	}
 	libHandlePtr := unsafe.Pointer(libHandle)
 
@@ -74,7 +77,7 @@ func LoadJVMLib(jvmLibPath string) (*LibJVMPtr, error) {
 	ptr := C.dlsym(libHandlePtr, (*C.char)(cs2))
 	if ptr == nil {
 		unloadLibJvm(libHandlePtr)
-		return nil, errors.New("could not find JNI_GetDefaultJavaVMInitArgs in libjvm.so")
+		return errors.New("could not find JNI_GetDefaultJavaVMInitArgs in libjvm.so")
 	}
 	C.var_JNI_GetDefaultJavaVMInitArgs = C.type_JNI_GetDefaultJavaVMInitArgs(ptr)
 
@@ -83,8 +86,8 @@ func LoadJVMLib(jvmLibPath string) (*LibJVMPtr, error) {
 	ptr = C.dlsym(libHandlePtr, (*C.char)(cs3))
 	if ptr == nil {
 		unloadLibJvm(libHandlePtr)
-		return nil, errors.New("could not find JNI_CreateJavaVM in libjvm.so")
+		return errors.New("could not find JNI_CreateJavaVM in libjvm.so")
 	}
 	C.var_JNI_CreateJavaVM = C.type_JNI_CreateJavaVM(ptr)
-	return &LibJVMPtr{p: libHandlePtr}, nil
+	return nil
 }
